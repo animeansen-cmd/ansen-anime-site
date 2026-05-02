5b3712a495ddc1c0aeb19d90610f67724e5764ef
animeansen-cmd <animeansen@gmail.com>
2026-05-02 18:42:18 -0300

    update site

--- Site/dist/assets/vendor-icons-D0a2OCZY.js
+++ Site/dist/assets/vendor-icons-D0a2OCZY.js
@@ -1,4 +1,4 @@
-import{r as o}from"./vendor-react-BDJNq4VR.js";/**
+import{r as o}from"./vendor-react-DZSIeqxb.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
